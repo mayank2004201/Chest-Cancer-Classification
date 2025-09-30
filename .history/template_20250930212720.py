3f4fe7dcc4a50e41e4621f8e0dcd3ad5e1d106b2
Mayank Goel <mayankgoel201@gmail.com>
2025-09-30 21:50:13 +0530

    Folder Structure
    Created different folders to use for different purposes with in the project folder

---
+++ .history/template_20250930212720.py
@@ -0,0 +1,28 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(Level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+project_name = "Chest Cancer Classification"
+
+
+list_of_files = [
+    
+'.github/workflows/.gitkeep",
+f"src/{project_name}/____init__.py",
+f"src/{project_name}/components/__init__.py",
+f"src/{project_name}/utils/__init__.py",
+f"src/{project_name}/config/__init__.py",
+f"src/{project_name}/config/configuration.py",
+ƒ"src/{project_name}/pipeline/__init__.py",
+f"src/{project_name}/entity/__init__.py",
+ƒ"src/{project_name}/constants/__init__.py",
+"config/config.yaml",
+"dvc.yaml",
+"params.yaml",
+"requirements.txt",
+"setup.py",
+"research/trials.ipynb", 
+"templates/index.html"
+]
